a9cabe92121ce6888ad29aaabf6c0aae55c3dd73
Arnab <arnabmondal1308@gmail.com>
2025-07-28 00:14:54 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css';
+import Header from './components/layout/Header';
+import About from './components/others/About';
+import Hero from './components/others/Hero';
+
+function App() {
+  return (
+   <>
+    <Header/>
+    <Hero/>
+    <About/>
+   </>
+  );
+}
+
+export default App;
